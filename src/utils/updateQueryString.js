421e9126f770513e53b5a436422680774624bf02
Micah Stubbs <micah.stubbs@gmail.com>
2019-06-16 14:45:32 -0700

    encodeUriComponent when updating query string value

--- src/utils/updateQueryString.js
+++ src/utils/updateQueryString.js
@@ -2,7 +2,7 @@ export default function(key, value) {
   const url = new URL(window.location)
   const params = new URLSearchParams(url.search)
   if (value === null || value.length === 0) params.delete(key)
-  else params.set(key, value)
+  else params.set(key, encodeUriComponent(value))
 
   url.search = params.toString()
   // eslint-disable-next-line no-restricted-globals
