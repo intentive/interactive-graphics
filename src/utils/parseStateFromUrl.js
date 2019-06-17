08d54b583463ecda34bb3021c5cb4b29fac241dd
Micah Stubbs <micah.stubbs@gmail.com>
2019-06-16 23:49:05 -0700

    generalize parseStateFromUrl

--- src/utils/parseStateFromUrl.js
+++ src/utils/parseStateFromUrl.js
@@ -1,6 +1,6 @@
 import updateQueryStringBatch from './updateQueryStringBatch'
 
-export default function({ initialState }) {
+export default function({ initialState, ignoredKeys }) {
   // a whitelist of keys to look for
   const initialKeys = Object.keys(initialState)
 
@@ -8,23 +8,30 @@ export default function({ initialState }) {
   // parse out and apply any filters found there
   const url = new URL(window.location)
   const params = new URLSearchParams(url.search)
+  const newQueryStringState = {}
 
   // override starting values if they are found in the url
   for (let entry of params.entries()) {
     const key = decodeURIComponent(entry[0])
-    const value = decodeURIComponent(entry[1])
-    if (initialKeys.includes(key)) {
-      initialState[key] = value
+    if (ignoreKeys && ignoreKeys.includes(key)) {
+      continue
+    } else {
+      // the key is not ignored
+      // so we want it in the query string
+      const value = decodeURIComponent(entry[1])
+      if (initialKeys.includes(key)) {
+        // if we find the value in the query string
+        // update it
+        initialState[key] = value
+      }
+      newQueryStringState[key] = initialState[key]
     }
   }
 
   // update the query string
   // handle the case where one or both places
   // are initially absent from the query string
-  updateQueryStringBatch({
-    source: initialState.source,
-    compare: initialState.compare
-  })
+  updateQueryStringBatch({ newQueryStringState })
 
   return initialState
 }
